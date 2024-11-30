b9d338e39b2b825b4cf2a8198ce764683d299ec2
jocelyn-chang <jocelyn00jc@gmail.com>
2024-11-29 23:23:05 -0500

    Setup

---
+++ client/app/(tabs)/home.tsx
@@ -0,0 +1,40 @@
+import { Image, StyleSheet, Platform, ViewBase, View } from 'react-native';
+
+import { SafeAreaView } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={{flex: 1}}>
+        <SafeAreaView>
+            <View style={styles.wrapper}>
+
+            </View>
+        </SafeAreaView>
+    </ThemedView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: '100%',
+    backgroundColor: '#FFFFFF'
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
