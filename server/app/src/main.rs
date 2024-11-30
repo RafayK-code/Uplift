af5cfbff4dc655e4cf40e412ac5bc01b46cd0511
RafayK-code <kashifrafay075@gmail.com>
2024-11-29 22:57:15 -0500

    backend app now listening

--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use logger::LogLevel;
+
+async fn handler() -> &'static str {
+    "go away"
+}
+
+pub mod logger;
+
+#[tokio::main]
+async fn main() {
+    
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
+    log_msg!("MAIN", LogLevel::Info, "Listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
