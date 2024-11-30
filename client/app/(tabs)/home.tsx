9e9f69ee2ebe47ceeb924807abde5abfa3db8fb4
jocelyn-chang <jocelyn00jc@gmail.com>
2024-11-30 01:48:52 -0500

    edits

--- client/app/(tabs)/home.tsx
+++ client/app/(tabs)/home.tsx
@@ -1,7 +1,8 @@
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
-import { StyleSheet, SafeAreaView, View, TextInput, Pressable } from 'react-native'
+import { StyleSheet, SafeAreaView, View, TextInput, Pressable, Dimensions } from 'react-native'
 
+const { width, height } = Dimensions.get('window');
 
 import MainDisplay from './maindisplay';
 
@@ -26,7 +27,8 @@ export default function home() {
             <MainDisplay/>
 =======
             <View style={styles.mainContainer}>
-            <View style={styles.affirmation}>
+            <View style={styles.affirmation}></View>
+            <View style={StyleSheet.compose(styles.affirmation, {top: height * 0.3, left: width * 0.8})}>
               </View>
 >>>>>>> 870a7e244a9722cf3f8e0ffcef32bbd935c2d228
             </View>
@@ -41,6 +43,7 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
+      color: '#FFFFFF',
     },
   
 <<<<<<< HEAD
@@ -106,9 +109,11 @@ const styles = StyleSheet.create({
       marginTop: 10,
 =======
     affirmation: {
-      width: '6%', // 50% of the screen width
-      height: '20%', // 20% of the screen height
-      backgroundColor: '#4c669f', // Background color
+      width: '81%', // 50% of the screen width
+      height: '18%', // 20% of the screen height
+      top: height * 0.3, // 30% down from the top of the screen
+      right: width * 0.05, // 25% from the left
+      backgroundColor: '#000000', // Background color
       justifyContent: 'center', // Align text vertically
       alignItems: 'center', // Align text horizontally
       borderRadius: 10, // Optional: rounded corners
