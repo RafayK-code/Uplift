88c34f217284997449bdeed262d06e988a305c44
jocelyn-chang <jocelyn00jc@gmail.com>
2024-11-30 10:50:34 -0500

    scroll feature

--- client/app/(tabs)/home.tsx
+++ client/app/(tabs)/home.tsx
@@ -13,22 +13,45 @@ export default function home() {
         <ThemedView style={{ flex: 1 }}>
             <SafeAreaView/>
             <View style={styles.mainContainer}>
+            <ScrollView style={styles.whole}>
             <View style={styles.spacer}/>
+            <View style={styles.section1}>
+            <ThemedText style={styles.subtitle1}>
+              For You
+            </ThemedText>
             <ScrollView horizontal={true} style={styles.horizontalScroll}>
+            
             <View style={styles.affirmation1}>
-            {/* <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText> */}
+            <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText>
             </View>
             <View style={styles.affirmation1}>
             {/* <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText> */}
             </View>
             </ScrollView>
+            </View>
+            <View style={styles.section2}>
+            <ThemedText style={styles.subtitle1}>
+              For You
+            </ThemedText>
+            <ScrollView horizontal={true} style={styles.horizontalScroll}>
+            
+            <View style={styles.affirmation1}>
+            <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText>
+            </View>
+            <View style={styles.affirmation1}>
+            {/* <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText> */}
+            </View>
+            </ScrollView>
+            </View>
+            
+            </ScrollView>
       
-            <View style={styles.affirmation}>
+            {/* <View style={styles.affirmation}>
 
             </View>
             <View style={styles.affirmation}>
 
-            </View>
+            </View> */}
             </View>
             
         </ThemedView>
@@ -50,21 +73,22 @@ const styles = StyleSheet.create({
     },
 
     horizontalScroll: {
-      width: '80%',
+      width: '100%',
       height: '25%',
       flexDirection: 'row',
-      marginBottom: 2,
+      marginBottom: 5,
     },
     affirmation1: {
-      width: 200,
-      height: 150,
+      width: width * .8,
+      height: height*.25,
       flexDirection: 'row',
       backgroundColor: '#000000', // Background color
       // justifyContent: 'center', // Align text vertically
       // alignItems: 'center', // Align text horizontally
-      marginBottom: 15,
+      marginBottom: 5,
       borderRadius: 10, // Optional: rounded corners
-      marginLeft: 15
+      marginLeft: 15,
+      // marginTop: 150
     },
     affirmation: {
       width: '80%',
@@ -76,5 +100,22 @@ const styles = StyleSheet.create({
       marginBottom: 15,
       borderRadius: 10, // Optional: rounded corners
       marginLeft: 15
-    }
+    },
+    subtitle1: {
+      flexDirection: 'column',
+      marginBottom: 10,
+      color: 'black',
+      marginLeft: 15
+    },
+    section1: {
+      marginTop: 390,
+      flexDirection: 'column'
+    },
+    whole:{
+      flex: 1
+    },
+    section2: {
+      marginTop: 15,
+      flexDirection: 'column'
+    },
 });
