cadf70ab9cb9f2f946d3d61ff18ee6f37756b460
RafayK-code <kashifrafay075@gmail.com>
2024-11-29 23:53:20 -0500

    i hate this

--- server/database/initdb.sql
+++ server/database/initdb.sql
@@ -4,10 +4,10 @@ CREATE TABLE users(
 
 CREATE TABLE affirmations_generated(
     id SERIAL PRIMARY KEY,
-    user VARCHAR(255),
+    user_id VARCHAR(255),
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user) REFERENCES users(sub) ON DELETE RESTRICT
+    FOREIGN KEY (user_id) REFERENCES users(sub) ON DELETE RESTRICT
 );
 
 CREATE TABLE affirmations_mail(
@@ -23,21 +23,21 @@ CREATE TABLE affirmations_mail(
 
 CREATE TABLE affirmations_reactions(
     id SERIAL PRIMARY KEY,
-    user VARCHAR(255),
+    user_id VARCHAR(255),
     affirmation INT,
     reacted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (user) REFERENCES users(sub) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(sub) ON DELETE CASCADE,
     FOREIGN KEY (affirmation) REFERENCES affirmations_mail(id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_streaks(
     id SERIAL PRIMARY KEY,
-    user VARCHAR(255),
+    user_id VARCHAR(255),
     current_streak INT DEFAULT 0,
     last_streak_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     longest_streak INT DEFAULT 0
     affirmation_1 TEXT NOT NULL,
     affirmation_2 TEXT NOT NULL,
     affirmation_3 TEXT NOT NULL,
-    FOREIGN KEY (user) REFERENCES users(sub) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(sub) ON DELETE CASCADE
 );
