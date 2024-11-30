73b32b251b3399b85101b9579016d8cd6fd85b98
jocelyn-chang <jocelyn00jc@gmail.com>
2024-11-30 09:50:15 -0500

    blocks

--- client/app/(tabs)/home.tsx
+++ client/app/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
-import { StyleSheet, SafeAreaView, View, TextInput, Pressable, Dimensions } from 'react-native'
+import { StyleSheet, SafeAreaView, View, TextInput, Pressable, Dimensions, ScrollView } from 'react-native'
 
 const { width, height } = Dimensions.get('window');
 
@@ -12,8 +12,16 @@ export default function home() {
     return (
         <ThemedView style={{ flex: 1 }}>
             <SafeAreaView/>
+            <View style={styles.mainContainer}>
+            <View style={styles.spacer}/>
+            <ScrollView horizontal={true} style={styles.horizontalScroll} >
+            <View style={styles.affirmation}>
+            <ThemedText style={{ color: 'white' }}>Affirmation 1</ThemedText>
+            </View>
+            </ScrollView>
             <View style={styles.affirmation}>
 
+            </View>
             </View>
             
         </ThemedView>
@@ -25,17 +33,31 @@ const styles = StyleSheet.create({
     mainContainer: {
       flex: 1,
       flexDirection: 'column',
-      alignItems: 'center',
+      // alignItems: 'center',
       justifyContent: 'center',
       color: '#FFFFFF',
     },
+
+    spacer: {
+      flex: 1
+    },
+
+    horizontalScroll: {
+      width: '80%',
+      height: '75%',
+      flexDirection: 'column',
+      marginBottom: 15,
+    },
   
     affirmation: {
-      width: '100%', // 50% of the screen width
-      height: '18%', // 20% of the screen height
+      width: '80%',
+      height: '25%',
+      flexDirection: 'column',
       backgroundColor: '#000000', // Background color
-      justifyContent: 'center', // Align text vertically
-      alignItems: 'center', // Align text horizontally
+      // justifyContent: 'center', // Align text vertically
+      // alignItems: 'center', // Align text horizontally
+      marginBottom: 15,
       borderRadius: 10, // Optional: rounded corners
+      marginLeft: 15
     }
 });
