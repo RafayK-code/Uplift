4a9c16879a13a1aca459acdc3d5431748bca6629
RafayK-code <kashifrafay075@gmail.com>
2024-11-30 23:14:07 -0500

    add useStreak hook to connect frontend to backend endpoints

---
+++ client/hooks/useStreaks.ts
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useAuthFetch } from "./useAuthFetch"
+
+export interface StreakResponse {
+    currentStreak: number,
+    lastStreakDate: Date,
+    longestStreak: number,
+    affirmations: string[],
+}
+
+export interface StreakPayload {
+    currentStreak: number,
+    longestStreak: number,
+    content: string,
+}
+
+export const useStreaks = () => {
+    const { authFetch } = useAuthFetch();
+    const [loading, setLoading] = useState<boolean>();
+    const [streakResponseData, setStreakResponseData] = useState<StreakResponse | null>(null);
+
+    const serverUrl = 'http://localhost:8080';
+
+    const getStreakInfo = async () => {
+        setLoading(true);
+        try {
+            const data = await authFetch(`${serverUrl}/get_user_streak`, {
+                method: 'get',
+            })
+
+            const res: StreakResponse = {
+                currentStreak: data.current_streak,
+                lastStreakDate: new Date(data.last_streak_date),
+                longestStreak: data.longest_streak,
+                affirmations: data.affirmations,
+            }
+
+            setStreakResponseData(res);
+            console.log(res);
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    const checkIfSameAffirmation = (affirmation: string) => {   
+        if (streakResponseData != null) {
+            for (var i = 0; i < streakResponseData.affirmations.length; i++) {
+                if (affirmation === streakResponseData.affirmations[i])
+                    return true; 
+            }
+        }
+
+        return false;
+    }
+
+    const uploadStreakInfo = async (payload: StreakPayload) => {
+        setLoading(true);
+        try {
+            const data = await authFetch(`${serverUrl}/update_user_streak`, {
+                method: 'post',
+                data: {
+                    current_streak: payload.currentStreak,
+                    longest_streak: payload.longestStreak,
+                    content: payload.content,
+                }
+            })
+
+            const result = data.result.success;
+            if (!result)
+                throw new Error("failed to submit records");
+        }
+        catch(error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return { getStreakInfo, uploadStreakInfo, checkIfSameAffirmation };
+}
