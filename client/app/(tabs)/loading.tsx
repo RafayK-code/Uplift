cf13b33dbaf3fed1f65093496bef231b5976c0f2
jocelyn-chang <jocelyn00jc@gmail.com>
2024-12-01 00:23:49 -0500

    dunno man

--- client/app/(tabs)/loading.tsx
+++ client/app/(tabs)/loading.tsx
@@ -1,80 +1,41 @@
-import { Image, StyleSheet, SafeAreaView, View, Pressable} from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect, useState } from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useRouter } from 'expo-router';
+// import { Image, StyleSheet, SafeAreaView, View, Pressable, ActivityIndicator} from 'react-native';
+// import { ThemedText } from '@/components/ThemedText';
+// import { ThemedView } from '@/components/ThemedView';
+// import { useAuth0 } from '@auth0/auth0-react';
+// import { useEffect, useState } from 'react';
+// import { LinearGradient } from 'expo-linear-gradient';
+// import { useRouter } from 'expo-router';
 
 
-import { Colors } from '@/constants/Colors';
-import { navigate } from 'expo-router/build/global-state/routing';
+// import { Colors } from '@/constants/Colors';
+// import { navigate } from 'expo-router/build/global-state/routing';
 
-export default function LoadingScreen() {
-
-  return (
-    <ThemedView style={{ flex: 1 }}>
-      <SafeAreaView/>
+// export default function LoadingScreen() {
+//     if (loading) {
+//         return (
+//             <View style={styles.streakContainer}>
+//                 <ActivityIndicator size="large" />
+//             </View>
+//         );
+//     }
+// //   return (
+// //     <ThemedView style={{ flex: 1 }}>
+// //       <SafeAreaView/>
       
-    </ThemedView>
-  );
-}
+// //     </ThemedView>
+// //   );
+// }
 
-const styles = StyleSheet.create({
-  logoContainer: {
-    marginBottom: 5,
-  },
-  loginContainer: {
-    flex: 0.5,
-    width: '90%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: 10,
-    marginBottom: '-40%'
-  },
-  upliftLogo: {
-    width: 180,
-  },
+// const styles = StyleSheet.create({
+//     streakContainer: {
+//         marginTop: 30,
 
-  loginBtn: {
-    height: 50,
-    borderRadius: 8,
-    width: '100%',
-    marginVertical: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.light.text
-  },
-
-  btnText: {
-    color: 'white',
-  },
-
-  errorText: {
-    color: 'red',
-    marginTop: 10,
-  },
-gradientBackground: {
-  flex: 1, // Take up the full screen height
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexDirection: 'column',
-},
-logos: {
-  flex: 1, // Take up the full screen height
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexDirection: 'column',
-  marginTop: 75
-},
-message: {
-  textAlign: 'center',
-  fontSize: 14,
-  fontStyle: 'italic'
-},
-caption: {
-  textAlign: 'center',
-  padding: 50,
-  marginBottom: '-10%'
-}
-});
+//         width: 50,
+//         aspectRatio: 1,
+//         borderRadius: 50,
+//         backgroundColor: Colors.light.background,
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
